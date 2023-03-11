76a2ab9d0e2f6e08a4601713dfa0e783e74bccf7
minhye9731 <jenney62639731@gmail.com>
2023-03-12 00:09:19 +0900

    Initial Commit

---
+++ VIPPatternPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPPatternPractice
+//
+//  Created by 강민혜 on 3/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
