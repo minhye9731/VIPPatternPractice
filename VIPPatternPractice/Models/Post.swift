d649c1ce83203ce9c812e462096ce810dfe211d4
minhye9731 <jenney62639731@gmail.com>
2023-03-12 00:26:56 +0900

    feat : Post 데이터 외부 entity 생성

---
+++ VIPPatternPractice/Models/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  VIPPatternPractice
+//
+//  Created by 강민혜 on 3/12/23.
+//
+
+import Foundation
+
+struct Post {
+    let id: UUID = UUID()
+    let title: String
+    let content: String
+}
